77e60a973ec891ad9fae8b02c8f112e8dd761961
yuroc5390 <gabrielchen@gwu.edu>
2018-12-18 11:53:18 -0500

    modify auto.bash to enable loop testing

--- auto.bash
+++ auto.bash
@@ -1,10 +1,10 @@
+#!/bin/bash
 USR=`logname`
 AFL_PATH="/home/$USR/workspace/protocol/tools/fuzz_target"
-PROG_PATH="/home/$USR/workspace/protocol/tools/selftls"
+PROG_PATH="/home/$USR/workspace/protocol/progs/selftls"
 NTHREAD=$1
-
-for ((i=0;i<$NTHREAD;i++))
+for (( i=0;i<$NTHREAD;i++ ))
 do
 out_dir=fuzzer"$i"
-$AFL_PATH/experimental/asan_cgroups/limit_memory.sh -u $USR $AFL_PATH/afl-fuzz  -i $PROG_PATH/in/p1 -o $PROG_PATH/out_target -M $out_dir -m none -E 1 $PROG_PATH/selftls 1 @@ 
+$AFL_PATH/experimental/asan_cgroups/limit_memory.sh -u $USR $AFL_PATH/afl-fuzz  -i $PROG_PATH/in/p1 -o $PROG_PATH/out_target -M $out_dir -m none -E 1 $PROG_PATH/selftls 1 @@ &
 done
